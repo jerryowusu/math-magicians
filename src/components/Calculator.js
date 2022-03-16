158bbd4ba26b0c04016f256f8438aafbfd081e40
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-16 15:37:03 +0000

    refactor calculator class componet with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,56 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
+  const handleEvent = (e) => {
+    setState({ ...state, ...calculate(state, e.target.name) });
+  };
 
-    this.handleEvent = this.handleEvent.bind(this);
-  }
-
-  handleEvent = ((buttonName) => {
-    this.setState((state) => calculate(state, buttonName));
-  })
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="calculator">
-        <div className="display">
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </div>
-        <div className="figures">
-          <button type="button" className="clear num" name="AC" onClick={(e) => this.handleEvent(e.target.name)}>AC</button>
-          <button type="button" className="ad num" name="+/-" onClick={(e) => this.handleEvent(e.target.name)}>+/-</button>
-          <button type="button" className="modulo num" name="%" onClick={(e) => this.handleEvent(e.target.name)}>%</button>
-          <button type="button" className="divide operator" name="÷" onClick={(e) => this.handleEvent(e.target.name)}>÷</button>
-          <button type="button" className="num" name="7" onClick={(e) => this.handleEvent(e.target.name)}>7</button>
-          <button type="button" className="num" name="8" onClick={(e) => this.handleEvent(e.target.name)}>8</button>
-          <button type="button" className="num" name="9" onClick={(e) => this.handleEvent(e.target.name)}>9</button>
-          <button type="button" className="clear operator" name="x" onClick={(e) => this.handleEvent(e.target.name)}>x</button>
-          <button type="button" className="num" name="4" onClick={(e) => this.handleEvent(e.target.name)}>4</button>
-          <button type="button" className="num" name="5" onClick={(e) => this.handleEvent(e.target.name)}>5</button>
-          <button type="button" className="num" name="6" onClick={(e) => this.handleEvent(e.target.name)}>6</button>
-          <button type="button" className="clear operator" name="-" onClick={(e) => this.handleEvent(e.target.name)}>&ndash;</button>
-          <button type="button" className="num" name="1" onClick={(e) => this.handleEvent(e.target.name)}>1</button>
-          <button type="button" className="num" name="2" onClick={(e) => this.handleEvent(e.target.name)}>2</button>
-          <button type="button" className="num" name="3" onClick={(e) => this.handleEvent(e.target.name)}>3</button>
-          <button type="button" className="add operator" name="+" onClick={(e) => this.handleEvent(e.target.name)}>+</button>
-          <button type="button" className="zero" name="0" onClick={(e) => this.handleEvent(e.target.name)}>0</button>
-          <button type="button" className="num" name="." onClick={(e) => this.handleEvent(e.target.name)}>.</button>
-          <button type="button" className="equals operator" name="=" onClick={(e) => this.handleEvent(e.target.name)}>=</button>
-        </div>
+  return (
+    <div className="calculator">
+      <div className="display">
+        <span>{state.total}</span>
+        <span>{state.operation}</span>
+        <span>{state.next}</span>
       </div>
-    );
-  }
-}
+      <div className="figures">
+        <button type="button" className="clear num" name="AC" onClick={handleEvent}>AC</button>
+        <button type="button" className="ad num" name="+/-" onClick={handleEvent}>+/-</button>
+        <button type="button" className="modulo num" name="%" onClick={handleEvent}>%</button>
+        <button type="button" className="divide operator" name="÷" onClick={handleEvent}>÷</button>
+        <button type="button" className="num" name="7" onClick={handleEvent}>7</button>
+        <button type="button" className="num" name="8" onClick={handleEvent}>8</button>
+        <button type="button" className="num" name="9" onClick={handleEvent}>9</button>
+        <button type="button" className="clear operator" name="x" onClick={handleEvent}>x</button>
+        <button type="button" className="num" name="4" onClick={handleEvent}>4</button>
+        <button type="button" className="num" name="5" onClick={handleEvent}>5</button>
+        <button type="button" className="num" name="6" onClick={handleEvent}>6</button>
+        <button type="button" className="clear operator" name="-" onClick={handleEvent}>&ndash;</button>
+        <button type="button" className="num" name="1" onClick={handleEvent}>1</button>
+        <button type="button" className="num" name="2" onClick={handleEvent}>2</button>
+        <button type="button" className="num" name="3" onClick={handleEvent}>3</button>
+        <button type="button" className="add operator" name="+" onClick={handleEvent}>+</button>
+        <button type="button" className="zero" name="0" onClick={handleEvent}>0</button>
+        <button type="button" className="num" name="." onClick={handleEvent}>.</button>
+        <button type="button" className="equals operator" name="=" onClick={handleEvent}>=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
