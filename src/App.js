7bb07b19fe514a31674165eb0c32bb233a882908
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-22 21:30:01 +0000

    update nav bar links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import NotMatch from './pages/NotMatch';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/calculator">
+          <Calculator />
+        </Route>
+        <Route path="/quote">
+          <Quote />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </>
+  );
 }
+
 export default App;
